3a772c133472f3c692d4d0b35fe43fb7444b17e4
rickyshiv <rickyshivd10@gmail.com>
2022-06-22 17:58:46 +0530

    Josephus Problem Using Recursion

---
+++ src/Josephus.java
@@ -0,0 +1,25 @@
+
+public class Josephus {
+
+
+
+	static int josephus(int n, int k)
+	{
+		if (n == 1)
+			return 0;
+		else
+			
+			return (josephus(n - 1, k) + k) % n;
+	}
+
+	
+	public static void main(String[] args)
+	{
+		int n = 5;
+		int k = 3;
+		System.out.println("The chosen place is "
+						+ josephus(n, k));
+	}
+}
+
+
--- src/kadens_algo.java
+++ src/kadens_algo.java
@@ -2,13 +2,20 @@ public class kadens_algo {
     
     static int maxSubArraySum(int a[], int size)
     {
-    int max_so_far = a[0];
-    int curr_max = a[0];
+    int max_so_far = Integer.MIN_VALUE;
+    int curr_max = 0;
 
-    for (int i = 1; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        curr_max = Math.max(a[i], curr_max+a[i]);
-        max_so_far = Math.max(max_so_far, curr_max);
+    	curr_max=curr_max+a[i];
+    	if(curr_max>max_so_far)
+    	{
+    		max_so_far=curr_max;
+    	}
+    	if(curr_max<0)
+    	{
+    		curr_max=0;
+    	}
     }
     return max_so_far;
     }
